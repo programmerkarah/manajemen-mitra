6eeff52ef9c9ca1d300746327b2f32372d2b8275
programmerkarah <rahmatzikribps@gmail.com>
2025-12-14 02:47:38 +0700

    remove database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,12 +21,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
+            
         );
     }
 }
