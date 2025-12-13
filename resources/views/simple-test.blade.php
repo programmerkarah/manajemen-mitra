f5c4fc3b29da05416dd5cebc7c645ec8f321da72
programmerkarah <rahmatzikribps@gmail.com>
2025-12-14 06:26:36 +0700

    Add simple Blade test view

---
+++ resources/views/simple-test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Test</title>
+</head>
+<body>
+    <h1>✅ Laravel View Works!</h1>
+    <p>If you see this, Laravel can render views.</p>
+    <p><a href="/">Try Inertia Home Page</a></p>
+    <p><a href="/login">Try Login Page</a></p>
+</body>
+</html>
