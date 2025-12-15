4e72323ca79cedd1fe749c597a6ffe4023e5f0cf
programmerkarah <rahmatzikribps@gmail.com>
2025-12-15 23:43:30 +0700

    overhaul user interface

---
+++ resources/lang/id/fortify.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'two_factor' => [
+        'invalid_code' => 'Kode autentikasi dua faktor yang dimasukkan tidak valid.',
+        'invalid_recovery_code' => 'Recovery code dua faktor yang dimasukkan tidak valid.',
+    ],
+    // Tambahkan terjemahan lain sesuai kebutuhan
+    'The provided two factor authentication code was invalid.' => 'Kode autentikasi dua faktor yang dimasukkan tidak valid.',
+    'The provided two factor recovery code was invalid.' => 'Recovery code dua faktor yang dimasukkan tidak valid.',
+];
