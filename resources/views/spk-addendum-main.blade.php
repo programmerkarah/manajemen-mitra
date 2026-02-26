5365aa68d5072246937554a80a3baeee912f443a
Rahmat <rahmatzikribps@gmail.com>
2026-02-26 13:17:18 +0700

    perubahan template addendum

--- resources/views/spk-addendum-main.blade.php
+++ resources/views/spk-addendum-main.blade.php
@@ -291,7 +291,7 @@
 
         <p class="dasar" style="margin-bottom: 10px;">
             Dasar Perubahan/{{ $addendumLabel }} Perjanjian ini yaitu:<br>
-            Terjadi perubahan realisasi pelaksanaan kegiatan yang dilakukan oleh <strong>PIHAK KEDUA</strong> sebagaimana tercantum dalam lampiran {{$addendumLabel}} yang disebabkan adanya {{ $hasUbinanKegiatan ? ', dan/atau terjadi lewat panen pada kegiatan Ubinan Padi/Palawija' : '' }}, dan/atau terjadi perubahan wilayah kerja pada kegiatan survei yang mengakibatkan perubahan beban kerja dan/atau honorarium yang diterima oleh <strong>PIHAK KEDUA</strong>.
+            Terjadi perubahan realisasi pelaksanaan kegiatan yang dilakukan oleh <strong>PIHAK KEDUA</strong> sebagaimana tercantum dalam lampiran {{$addendumLabel}} yang disebabkan adanya {{ $hasUbinanKegiatan ? 'lewat panen pada kegiatan Ubinan Padi/Palawija, dan/atau terjadi perubahan wilayah kerja pada kegiatan survei' : 'perubahan wilayah kerja pada kegiatan survei' }},  yang mengakibatkan perubahan beban kerja dan/atau honorarium yang diterima oleh <strong>PIHAK KEDUA</strong>.
         </p>
 
 
