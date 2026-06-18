3ea8160d3d05a3510e07e30e1b4e44bd599bf893
programmerkarah <rahmatzikribps@gmail.com>
2026-06-18 13:02:34 +0700

    fix bug interface tombol re-generate

--- resources/js/components/ui/switch.tsx
+++ resources/js/components/ui/switch.tsx
@@ -10,6 +10,9 @@ const Switch = React.forwardRef<
   <SwitchPrimitives.Root
     className={cn(
       "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 bg-neutral-200 dark:bg-neutral-700",
+      // Color change when ON
+      "data-[state=checked]:bg-emerald-500 data-[state=checked]:border-emerald-500",
+      "dark:data-[state=checked]:bg-sky-500 dark:data-[state=checked]:border-sky-500",
       className
     )}
     {...props}
