d0d32b06ef6962bafd21149f500401dc8a03b738
programmerkarah <rahmatzikribps@gmail.com>
2026-09-23 23:23:46 +0700

    fix BAST card status, download file and zip

--- resources/js/pages/Bast/Show.tsx
+++ resources/js/pages/Bast/Show.tsx
@@ -14,10 +14,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem, type SharedData } from '@/types';
-import {
-    openFastDownload,
-    previewFileFromPost,
-} from '@/utils/downloadUtils';
+import { openFastDownload, previewFileFromPost } from '@/utils/downloadUtils';
 import { encryptFilters } from '@/utils/encryption';
 import { Head, Link, router, usePage } from '@inertiajs/react';
 import {
